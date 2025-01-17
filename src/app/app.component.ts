817bd90ffe3f7b4125e6b34f6fa0091d5207a146
Saumya <saumya.singh1702@gmail.com>
2025-01-16 21:16:01 -0500

    Decorators in typescript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,33 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+@Logger // Typescript Class Decorator
 export class AppComponent {
   title = 'personality-test';
+  constructor(){}
+
+  @Log
+  add(a: number, b: number){
+    return a+b;
+  }
 }
+// Function for class decorator
+function Logger(target: Function){
+  console.log(`Logging Class : ${target.name}`);
+}
+
+// Function for function decorator
+function Log(target: any, propertyKey: any, description: PropertyDescriptor){
+  const origMethod = description.value;
+
+  description.value = function(...args: any[]){
+    console.log(`Calling ${propertyKey} with arguments`, args);
+    return origMethod.apply(this, args);
+  }
+
+}
+
+const calc = new AppComponent();
+console.log(calc.add(4,5));
+
